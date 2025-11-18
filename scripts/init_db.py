f8fae9d23bcc4da2cdca7b8c949506c8567d4cd0
Ahmet Emre Parmaksiz <ahmetemreparmaksiz@gmail.com>
2025-11-18 23:22:48 +0300

    fix: db init

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-
-import sys
 import os
+import sys
 
-# --- Import path ayarı ---
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_DIR)
 
-from backend.database import init_db
+from backend.database import engine
+from backend.models.base import Base
 
 def main():
     print("== HIDS DB INIT ==")
-    print("veritabanı oluşturuluyor...")
+    print("Veritabanı oluşturuluyor...")
 
-    init_db()
+    Base.metadata.create_all(bind=engine)
 
-    print("Tablolar hazır.\n")
+    print("Tablolar hazır.")
 
 if __name__ == "__main__":
     main()
