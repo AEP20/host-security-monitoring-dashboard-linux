4b5866b70f8347ad51323acbbcf8be9d7b4a73a5
Ahmet Emre Parmaksiz <ahmetemreparmaksiz@gmail.com>
2025-11-18 23:28:15 +0300

    fix:path

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from backend.models.base import Base
 
-engine = create_engine("sqlite:///hids.db", echo=False)
+DB_PATH = "/opt/HIDS/state/hids.db"
+engine = create_engine(f"sqlite:///{DB_PATH}", echo=False)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
 def init_db():
