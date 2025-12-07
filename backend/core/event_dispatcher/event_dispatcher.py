2e2c6c40c3547576e87274aecb0ec7daa1121901
Ahmet Emre Parmaksiz <ahmetemreparmaksiz@gmail.com>
2025-12-07 16:46:12 +0300

    feat: network page

--- backend/core/event_dispatcher/event_dispatcher.py
+++ backend/core/event_dispatcher/event_dispatcher.py
@@ -4,7 +4,7 @@
 # from models.log_model import LogEventModel
 from backend.models.metric_model import MetricModel
 # from models.alert_model import AlertModel
-from models.network_event_model import NetworkEventModel
+from backend.models.network_event_model import NetworkEventModel
 from backend.database import save_metric_snapshot
 
 from backend.models.process_event_model import ProcessEventModel
